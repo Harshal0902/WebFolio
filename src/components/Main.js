f7cded1f1d165bb68675901ae1223b1a110f18e7
Your Name <hr5359@srmist.edu.in>
2021-04-16 22:37:49 +0530

    main component added

---
+++ src/components/Main.js
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+import {StyleProvider} from "../contexts/StyleContext";
+import "./Main.css";
+export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDark: false
+    };
+  }
+
+  componentDidMount() {
+    if (localStorage.getItem("isDark") === null) {
+      const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
+      localStorage.setItem("isDark", darkPref.matches);
+    }
+    this.setState({isDark: JSON.parse(localStorage.getItem("isDark"))});
+  }
+  changeTheme = () => {
+    this.setState({isDark: !this.state.isDark}, () => {
+      localStorage.setItem("isDark", this.state.isDark);
+    });
+  };
+
+  render(){
+    return (
+      <div className={this.state.isDark ? "dark-mode" : null}>
+        <StyleProvider
+          value={{isDark: this.state.isDark, changeTheme: this.changeTheme}}
+        >
+        </StyleProvider>
+      </div>
+    );
+  }
+}

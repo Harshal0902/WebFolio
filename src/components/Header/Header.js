42f903f762b1abf44e6dcc9b48ec2894c7d66ba0
Your Name <hr5359@srmist.edu.in>
2021-07-20 10:42:35 +0530

    bugs fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,82 +11,82 @@ function Header() {
 
   return (
     <Headroom style={{ position: "fixed" }}>
-      <Fade top>
-        <header className={isDark ? "dark__menu header" : "header"}>
-          <a href="/" className="logo">
-            <span className="grey__color">&lt;</span>
-            <span className="logo-name">John Doe</span><span className="grey__color">/&gt;</span>
-          </a>
-          <input className="menu__btn" for='menu__btn' type="checkbox" id="menu__btn" />
-          <label
-            className="menu__icon"
-            htmlFor="menu__btn"
-            style={{ color: "white" }}
-          >
-            <span className={isDark ? "navicon navicon-dark" : "navicon"}></span>
-          </label>
-          <ul className={isDark ? "dark-menu menu" : "menu"}>
-            <li>
-              <Link activeClass="active"
-                to="skills"
-                spy={true}
-                smooth={true}
-                offset={-50}
-                duration={250}>
-                Skills</Link>
-            </li>
-            <li>
-              <Link activeClass="active"
-                to="education"
-                spy={true}
-                smooth={true}
-                offset={-50}
-                duration={250}>
-                Education</Link>
-            </li>
-            <li>
-              <Link activeClass="active"
-                to="experiences"
-                spy={true}
-                smooth={true}
-                offset={-50}
-                duration={250}>
-                Work Experiences</Link>
-            </li>
-            <li>
-              <Link activeClass="active"
-                to="projects"
-                spy={true}
-                smooth={true}
-                offset={-50}
-                duration={250}>
-                Projects</Link>
-            </li>
-            <li>
-              <Link activeClass="active"
-                to="awards"
-                spy={true}
-                smooth={true}
-                offset={-50}
-                duration={250}>
-                Awards</Link>
-            </li>
-            <li>
-              <Link activeClass="active"
-                to="contact"
-                spy={true}
-                smooth={true}
-                offset={-50}
-                duration={250}>
-                Let's Talk</Link>
-            </li>
-            <li>
-              <a rel="Dark Mode Toggle" href="/#">
-                <ToggleSwitch />
-              </a>
-            </li>
-          </ul>
-        </header>
+      <Fade top duration="2000">
+      <header className={isDark ? "dark-menu header" : "header"}>
+        <a href="/" className="logo">
+          <span className="grey-color">&lt;</span>
+          <span className="logo-name">John Doe</span><span className="grey-color">/&gt;</span>
+        </a>
+        <input className="menu-btn" for='menu-btn' type="checkbox" id="menu-btn" />
+        <label
+          className="menu-icon"
+          htmlFor="menu-btn"
+          style={{ color: "white" }}
+        >
+          <span className={isDark ? "navicon navicon-dark" : "navicon"}></span>
+        </label>
+        <ul className={isDark ? "dark-menu menu" : "menu"}>
+          <li>
+            <Link activeClass="active"
+              to="skills"
+              spy={true}
+              smooth={true}
+              offset={-50}
+              duration={250}>
+              Skills</Link>
+          </li>
+          <li>
+            <Link activeClass="active"
+              to="education"
+              spy={true}
+              smooth={true}
+              offset={-50}
+              duration={250}>
+              Education</Link>
+          </li>
+          <li>
+            <Link activeClass="active"
+              to="experiences"
+              spy={true}
+              smooth={true}
+              offset={-50}
+              duration={250}>
+              Work Experiences</Link>
+          </li>
+          <li>
+            <Link activeClass="active"
+              to="projects"
+              spy={true}
+              smooth={true}
+              offset={-50}
+              duration={250}>
+              Projects</Link>
+          </li>
+          <li>
+            <Link activeClass="active"
+              to="awards"
+              spy={true}
+              smooth={true}
+              offset={-50}
+              duration={250}>
+              Awards</Link>
+          </li>
+          <li>
+            <Link activeClass="active"
+              to="contact"
+              spy={true}
+              smooth={true}
+              offset={-50}
+              duration={250}>
+              Let's Talk</Link>
+          </li>
+          <li>
+            <a rel="Dark Mode Toggle" aria-label="Toggle" aria-labelledby="Dark Mode Toggle" href="/#">
+              <ToggleSwitch />
+            </a>
+          </li>
+        </ul>
+      </header>
       </Fade>
     </Headroom>
   );
